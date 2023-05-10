7ed75ccc31e65e787e8d0cc997ff3a0b7bcd6317
Shashi Kumar <132669257+shashi-kumar-17@users.noreply.github.com>
2023-05-10 23:05:36 +0530

    Update mail.php
    Changed the email address

--- mail.php
+++ mail.php
@@ -4,14 +4,14 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $subject1 = $_POST['subject1'];
 $message= $_POST['message'];
-$to = "shashi17kumar06@mail.com";
+$to = "shashi17kumar06@gmail.com";
 $subject = "Mail From Portfolio";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Subject =" . $subject1 . "\r\n Message =" . $message;
 $headers = "From: noreply@shashi1706.vercel.app" . "\r\n" .
-"CC: loremvampire17@example.com";
+"CC: loremvampire17@gmail.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
 //redirect
-echo("Thank you for contacting me.");
+echo "Thank you for contacting me.";
 ?>
