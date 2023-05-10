2a392a21e01427cbf048c69c3182e9683898b27a
Shashi Kumar <132669257+shashi-kumar-17@users.noreply.github.com>
2023-05-10 23:27:52 +0530

    Update mail.php code

--- mail.php
+++ mail.php
@@ -1,17 +1,66 @@
-<?php
-//get data from form  
-$name = $_POST['name'];
-$email = $_POST['email'];
-$subject1 = $_POST['subject1'];
-$message= $_POST['message'];
-$to = "shashi17kumar06@gmail.com";
-$subject = "Mail From Portfolio";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Subject =" . $subject1 . "\r\n Message =" . $message;
-$headers = "From: noreply@shashi1706.vercel.app" . "\r\n" .
-"CC: loremvampire17@gmail.com";
-if($email!=NULL){
-    mail($to,$subject,$txt,$headers);
-}
-//redirect
-echo "Thank you for contacting me.";
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php 
+
+    if(isset($_POST['btn-send']))
+
+    {
+
+       $UserName = $_POST['UName'];
+
+       $Email = $_POST['Email'];
+
+       $Subject = $_POST['Subject'];
+
+       $Msg = $_POST['msg'];
+
+       if(empty($UserName) || empty($Email) || empty($Subject) || empty($Msg))
+
+       {
+
+           header('location:contactMe.html?error');
+
+       }
+
+       else
+
+       {
+
+           $to = "shashi17kumar06@gmail.com";
+
+           if(mail($to,$Subject,$Msg,$Email))
+
+           {
+
+               header("location:contactMe.html?success");
+
+           }
+
+       }
+
+    }
+
+    else
+
+    {
+
+        header("location:contactMe.html");
+
+    }
+
 ?>
